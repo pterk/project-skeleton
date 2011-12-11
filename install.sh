9f9e4e71226532d2f782acf9a3fa7c81ab5bd2fe
Peter van Kampen <pterk@datatailors.com>
2011-12-11 20:16:14 +0100

    Skip zeromq for now, add django bash completion

--- install.sh
+++ install.sh
@@ -10,6 +10,19 @@ if [ ! -f requirements.txt ]; then
     exit
 fi
 
-./install_zeromq.sh
+#./install_zeromq.sh
 pip install -r requirements.txt
-pip install -r requirements-ztask.txt --install-option="--zmq=${VIRTUAL_ENV}"
+#pip install -r requirements-ztask.txt --install-option="--zmq=${VIRTUAL_ENV}"
+
+wget http://code.djangoproject.com/svn/django/trunk/extras/django_bash_completion -O ${VIRTUAL_ENV}/bin/django_bash_completion
+
+POSTACTIVATE=${VIRTUAL_ENV}/bin/postactivate
+
+if [ -x ${POSTACTIVATE} ]; then
+    echo -e 'Adding django_bash_completion to postactivate'
+    echo -e 'source ${VIRTUAL_ENV}/bin/django_bash_completion' >> ${POSTACTIVATE}
+else
+    echo -e 'Creating postactivate'
+    echo -e '#!/bin/bash\nsource ${VIRTUAL_ENV}/bin/django_bash_completion' >> ${POSTACTIVATE}
+    chmod 755 ${POSTACTIVATE}
+fi
